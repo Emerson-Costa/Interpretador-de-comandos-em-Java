866a02a9ad79db142360370352035bea0f393c84
Karina Fedrizzi <49348650+K-fedrizzi@users.noreply.github.com>
2020-10-28 14:29:43 -0300

    Alteracoes

--- Interpretador.java
+++ Interpretador.java
@@ -6,34 +6,71 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
 /**
-* Trabalho de SO
-**/
-class Interpretador{
+ * Trabalho de SO
+ * https://www.softorks.com/en/java/how-to-execute-shell-command-java-runtimeExec.php
+ **/
+class Interpretador {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args)  throws IOException, InterruptedException{
+       Interpretador i = new Interpretador();
+       i.primeiro();
+    }
 
-       String [] comand = {"ping", "google.com"};
+    public void segundo(){
+        try {
+            Process process = Runtime.getRuntime().exec("ls -l /");
 
-       ProcessBuilder processBuilder = new ProcessBuilder(comand);
-       processBuilder.directory(new File(System.getProperty("user.home")));
-       
-       try {
-           Process process = processBuilder.start();
+            StringBuilder output = new StringBuilder();
 
-           BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-           String line;
+            String line;
 
-           while((line = reader.readLine()) != null){
-                System.out.print(line +"\n");
-           }
+            while((line = reader.readLine()) != null){
+                output.append(line + "\n");
+            }
 
-           int exitCode = process.waitFor();
-
-           System.out.println("\n exited whith error code:" + exitCode);
-
-       } catch(IOException e) {
+            int exitVal = process.waitFor();
+            if(exitVal == 0){
+                System.out.println("Sucesso");
+                System.out.println(output);
+                System.exit(0);
+            }else{
+                System.out.println("Som");
+            }
+            
+        } catch (IOException e) {
            e.printStackTrace();
-       }
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    // metodos 
+    public void primeiro()  throws IOException, InterruptedException {
+
+        String[] comand = { "ping", "google.com" };
+
+        ProcessBuilder processBuilder = new ProcessBuilder(comand);
+        processBuilder.directory(new File(System.getProperty("user.home")));
+
+        try {
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.print(line + "\n");
+            }
+
+            int exitCode = process.waitFor();
+
+            System.out.println("\n exited whith error code:" + exitCode);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
