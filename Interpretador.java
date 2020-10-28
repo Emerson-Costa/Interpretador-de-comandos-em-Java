2bfe96e12643becf5479dca6acba07fec772586e
Karina Fedrizzi <49348650+K-fedrizzi@users.noreply.github.com>
2020-10-27 21:34:00 -0300

    Codigo do inicio do projeto

--- Interpretador.java
+++ Interpretador.java
@@ -1,11 +1,39 @@
 import java.lang.System;
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
 /**
 * Trabalho de SO
 **/
 class Interpretador{
 
-    public static void main(String[] args){
-        System.out.println("Ola Mundo!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+       String [] comand = {"ping", "google.com"};
+
+       ProcessBuilder processBuilder = new ProcessBuilder(comand);
+       processBuilder.directory(new File(System.getProperty("user.home")));
+       
+       try {
+           Process process = processBuilder.start();
+
+           BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+           String line;
+
+           while((line = reader.readLine()) != null){
+                System.out.print(line +"\n");
+           }
+
+           int exitCode = process.waitFor();
+
+           System.out.println("\n exited whith error code:" + exitCode);
+
+       } catch(IOException e) {
+           e.printStackTrace();
+       }
     }
 }
